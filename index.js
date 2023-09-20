16df7f1ab04260c64ca6d7da1d42039ad261629c
BekahHW <rhawrot@gmail.com>
2023-09-20 13:32:04 -0400

    Added lots of things

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import stripAnsi from "strip-ansi";
+// const chalk = require("chalk");
+
+const warning = chalk.hex("#FFA500");
+function logLines(lines) {
+    const longestLineLength = lines.reduce(
+      (biggest, line) => Math.max(biggest, stripAnsi(line).length),
+      0
+    );
+  
+    console.log("-".repeat(longestLineLength + 4));
+    for (const line of lines) {
+      console.log(`| ${line} ${" ".repeat(longestLineLength - stripAnsi(line).length)}|`);
+    }
+    console.log("-".repeat(longestLineLength + 4));
+  }
+  
+  logLines([
+chalk.blue("hello world!"),
+warning("this is a warning"),
+chalk.red("Hi, I'm BekahHW, DevEx Lead at OpenSauced 🍕")
+  ])
